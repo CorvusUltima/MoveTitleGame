3c276f2f7807e1ec6ea83254b117b06cd19b620d
BojanPav <77965118+BojanPav@users.noreply.github.com>
2021-01-26 10:04:10 +0100

    modification of new branch

--- src/Player.java
+++ src/Player.java
@@ -1,26 +1,18 @@
 import java.util.concurrent.ThreadLocalRandom;
-
 public class Player {
 
 
     String Name;
     boolean TurnToPlay;
 
-    public static String Random() {
+    public static int Random() {
         int diceCast = ThreadLocalRandom.current().nextInt(1, 6 + 1);
-        if (diceCast == 1) {
-            return "1";
-        } else if (diceCast == 2) {
-            return "2";
-        } else if (diceCast == 3) {
-            return "3";
-        } else if (diceCast == 4) {
-            return "4";
-        } else if (diceCast == 5) {
-            return "5";
-        } else if (diceCast == 6) {
-            return "6";
-        } else return "Error";
+      return diceCast;
+    }
+
+
+    public static void PlayerName(String name, int num) {
+        System.out.println("Player" + " " + num + " " + "is on the turn");
     }
 }
 
