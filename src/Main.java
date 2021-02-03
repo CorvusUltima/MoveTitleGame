6550f8aa2bf6e3222ce760c7b081981a95fcef30
BojanPav <77965118+BojanPav@users.noreply.github.com>
2021-02-03 13:33:07 +0100

    testing

--- src/Main.java
+++ src/Main.java
@@ -7,42 +7,17 @@ public class Main {
 
         Player player1=new Player();
         Player player2=new Player();
-        PlayerNames(player1,player2);
-        FirstToPlay(player1,player2);
+        Player.PlayerNames(player1,player2);
+        Player.FirstToPlay(player1,player2);
         String movie = Movie.randomise();
 
 
         System.out.println("movie is " + movie);
+
     }
 
-    public static int DiceCast(){
-        int diceNum=ThreadLocalRandom.current().nextInt(1, 5 + 1);
-        return diceNum;
-    }
-    public static void PlayerNames(Player player1,Player player2){
-        Scanner input = new Scanner(System.in);
-        System.out.print("player 1 name:"+"\n");
-        player1.name = input.nextLine();
-        System.out.print("player 2 name:"+"\n");
-        player2.name = input.nextLine();
-    }
-    public static void FirstToPlay(Player player1,Player player2) {
-        System.out.print(player1.name + "\t" + "\n" + "it's your turn to roll the dice good luck" + "\n");
-        player1.diceNum = DiceCast();
-        System.out.print(player1.name + "\t" + "your dice cast is " + "\t" + player1.diceNum + "\n");
-        System.out.print(player2.name + "\t" + "it's your turn to roll the dice good luck" + "\n");
-        player2.diceNum = DiceCast();
-        System.out.print(player2.name + "\t" + "your dice cast is" + "\t" + player2.diceNum + "\n");
-        if (player1.diceNum < player2.diceNum) {
-            System.out.println(player2.diceNum +" " + "will go first :");
-        } else if (player1.diceNum > player2.diceNum) {
-            System.out.println(player1.name + " " + "goes first");
-        }
-        else if (player1.diceNum== player2.diceNum)
-        {
-            System.out.print("\n" + "Draw, let's go one more time. "+"\n");
 
-            FirstToPlay( player1, player2);
-        }
+
+
     }
-}
+
--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,40 @@
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Player {
 
         String name;
         int diceNum;
 
+        public static void PlayerNames(Player player1, Player player2) {
+                Scanner input = new Scanner(System.in);
+                System.out.print("player 1 name:" + "\n");
+                player1.name = input.nextLine();
+                System.out.print("player 2 name:" + "\n");
+                player2.name = input.nextLine();
+        }
 
+        public static int DiceCast() {
+                int diceNum = ThreadLocalRandom.current().nextInt(1, 5 + 1);
+                return diceNum;
+        }
 
+        public static void FirstToPlay(Player player1, Player player2) {
+                System.out.print(player1.name + "\t" + "\n" + "it's your turn to roll the dice good luck" + "\n");
+                player1.diceNum = DiceCast();
+                System.out.print(player1.name + "\t" + "your dice cast is " + "\t" + player1.diceNum + "\n");
+                System.out.print(player2.name + "\t" + "it's your turn to roll the dice good luck" + "\n");
+                player2.diceNum = DiceCast();
+                System.out.print(player2.name + "\t" + "your dice cast is" + "\t" + player2.diceNum + "\n");
+                if (player1.diceNum < player2.diceNum) {
+                        System.out.println(player2.diceNum + " " + "will go first :");
+                } else if (player1.diceNum > player2.diceNum) {
+                        System.out.println(player1.name + " " + "goes first");
+                } else if (player1.diceNum == player2.diceNum) {
+                        System.out.print("\n" + "Draw, let's go one more time. " + "\n");
+
+                        FirstToPlay(player1, player2);
+                }
+
+        }
 }
