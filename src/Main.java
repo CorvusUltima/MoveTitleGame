ce03a9f85c7741f1d7a373dbcc58e085363219f5
BojanPav <77965118+BojanPav@users.noreply.github.com>
2021-02-15 08:59:13 +0100

    fixing break

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,92 @@
-import java.util.Locale;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.Scanner;
-import java.io.File;
-import java.io.IOException;
-import java.io.FileWriter;
 public class Main {
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        int gameround = 0;
         Player player1 = new Player();
         Player player2 = new Player();
         Player.PlayerNames(player1, player2);
-        Player.FirstToPlay(player1, player2);
         Movie movie = new Movie();
-        movie.SetAMovie();
-        Scanner input = new Scanner(System.in);
-        for (int i = 0; i < 5; i++) {
+        if (gameround == 0) {
+            Player.FirstToPlay(player1, player2);
+            movie.SetAMovie();
+            Scanner input = new Scanner(System.in);
+            for (int i = 0; i < 5; i++) {
 
-            if (player2.FirsttoPlay == true) {
-                System.out.println("Guess the movie name by hint : " + " " + movie.hints[i]);
-                System.out.println(player2.name + " " + "Enter your answer:");
-                player2.answer = input.nextLine();
-                if (player2.answer.equalsIgnoreCase(movie.name)) {
-                    System.out.println("Good guess!!");
-                    movie.SetAMovie();
+                if (player2.FirsttoPlay == true) {
+                    System.out.println("Guess the movie name by hint : " + " " + movie.hints[i]);
+                    System.out.println(player2.name + " " + "Enter your answer:");
+                    player2.answer = input.nextLine();
+                    if (player2.answer.equalsIgnoreCase(movie.name)) {
+                        System.out.println("Good guess!!");
+                        gameround++;
+                        break;
 
-                } else if (!player2.answer.equalsIgnoreCase(movie.name)) {
-                    System.out.println("your answer in not correct" +"\n"+ player1.name +" "+ "It is your chance now enter you answer:");
+                    } else if (!player2.answer.equalsIgnoreCase(movie.name)) {
+                        System.out.println("your answer in not correct" + "\n" + player1.name + " " + "It is your chance now enter you answer:");
+                        player1.answer = input.nextLine();
+                        player2.Points -= 10;
+                        if (player1.answer.equalsIgnoreCase(movie.name)) {
+                            System.out.println("Good guess!!");
+                            gameround++;
+                            break;
+                        } else if (!player1.answer.equalsIgnoreCase(movie.name)) {
+                            System.out.println("Both answers are wrong");
+                            player1.Points -= 10;
+                        }
+                    }
+
+                } else if (player1.FirsttoPlay == true) {
+                    System.out.println("Guess the movie name by hint : " + " " + movie.hints[i]);
+                    System.out.println(player1.name + " " + "Enter your answer:");
                     player1.answer = input.nextLine();
-                    player2.Points-=10;
+                    System.out.println(player1.answer);
                     if (player1.answer.equalsIgnoreCase(movie.name)) {
                         System.out.println("Good guess!!");
-                        movie.SetAMovie();
+                        gameround++;
+                        break;
                     } else if (!player1.answer.equalsIgnoreCase(movie.name)) {
-                        System.out.println("Both answers are wrong");
-                        player1.Points-=10;
+                        System.out.println("your answer in not correct" + "\n" + player2.name + " " + "It is your chance now enter you answer:");
+                        player2.answer = input.nextLine();
+                        player1.Points -= 10;
+                        if (player2.answer.equalsIgnoreCase(movie.name)) {
+                            System.out.println("Good guess!!");
+                            gameround++;
+                            break;
+                        } else if (!player2.answer.equalsIgnoreCase(movie.name)) {
+                            System.out.println("Both answers are wrong");
+                            player2.Points -= 10;
+                        }
+                    }
+                }
+            }
+            System.out.println(player1.name + " " + "have" + player1.Points + " " + player2.name + " " + "have" + player2.Points + " ");
+        } else if (gameround == 1) {
+            movie.SetAMovie();
+            Scanner input = new Scanner(System.in);
+            for (int i = 0; i < 5; i++) {
+
+                if (player2.FirsttoPlay == true) {
+                    System.out.println("Guess the movie name by hint : " + " " + movie.hints[i]);
+                    System.out.println(player2.name + " " + "Enter your answer:");
+                    player2.answer = input.nextLine();
+                    if (player2.answer.equalsIgnoreCase(movie.name)) {
+                        System.out.println("Good guess!!");
+                        gameround++;
+                        break;
+                    } else if (!player2.answer.equalsIgnoreCase(movie.name)) {
+                        System.out.println("your answer in not correct" + "\n" + player1.name + " " + "It is your chance now enter you answer:");
+                        player1.answer = input.nextLine();
+                        player2.Points -= 10;
+                        if (player1.answer.equalsIgnoreCase(movie.name)) {
+                            System.out.println("Good guess!!");
+                            gameround++;
+                            break;
+                        } else if (!player1.answer.equalsIgnoreCase(movie.name)) {
+                            System.out.println("Both answers are wrong");
+                            player1.Points -= 10;
+                        }
                     }
                 } else if (player1.FirsttoPlay == true) {
                     System.out.println("Guess the movie name by hint : " + " " + movie.hints[i]);
@@ -43,28 +95,89 @@ public class Main {
                     System.out.println(player1.answer);
                     if (player1.answer.equalsIgnoreCase(movie.name)) {
                         System.out.println("Good guess!!");
-                        movie.SetAMovie();
+                        gameround++;
+                        break;
                     } else if (!player1.answer.equalsIgnoreCase(movie.name)) {
-                        System.out.println("your answer in not correct"+"\n" + player2.name +" "+ "It is your chance now enter you answer:");
+                        System.out.println("your answer in not correct" + "\n" + player2.name + " " + "It is your chance now enter you answer:");
                         player2.answer = input.nextLine();
-                        player1.Points-=10;
+                        player1.Points -= 10;
                         if (player2.answer.equalsIgnoreCase(movie.name)) {
                             System.out.println("Good guess!!");
-                            movie.SetAMovie();
+                            gameround++;
+                            break;
                         } else if (!player2.answer.equalsIgnoreCase(movie.name)) {
                             System.out.println("Both answers are wrong");
-                            player2.Points-=10;
+                            player2.Points -= 10;
                         }
                     }
+                }
+            }
+            System.out.println(player1.name + " " + "have" + player1.Points + " " + player2.name + " " + "have" + player2.Points + " ");
+        }
+            else if (gameround == 2) {
+            movie.SetAMovie();
+            Scanner input = new Scanner(System.in);
+            for (int i = 0; i < 5; i++) {
+
+                if (player2.FirsttoPlay == true) {
+                    System.out.println("Guess the movie name by hint : " + " " + movie.hints[i]);
+                    System.out.println(player2.name + " " + "Enter your answer:");
+                    player2.answer = input.nextLine();
+                    if (player2.answer.equalsIgnoreCase(movie.name)) {
+                        System.out.println("Good guess!!");
+                        gameround++;
+                        break;
+
+                    } else if (!player2.answer.equalsIgnoreCase(movie.name)) {
+                        System.out.println("your answer in not correct" + "\n" + player1.name + " " + "It is your chance now enter you answer:");
+                        player1.answer = input.nextLine();
+                        player2.Points -= 10;
+                        if (player1.answer.equalsIgnoreCase(movie.name)) {
+                            System.out.println("Good guess!!");
+                            gameround++;
+                            break;
+                        } else if (!player1.answer.equalsIgnoreCase(movie.name)) {
+                            System.out.println("Both answers are wrong");
+                            player1.Points -= 10;
+                        }
+                    }
+                }
+                    else if (player1.FirsttoPlay == true) {
+                        System.out.println("Guess the movie name by hint : " + " " + movie.hints[i]);
+                        System.out.println(player1.name + " " + "Enter your answer:");
+                        player1.answer = input.nextLine();
+                        System.out.println(player1.answer);
+                        if (player1.answer.equalsIgnoreCase(movie.name)) {
+                            System.out.println("Good guess!!");
+                            gameround++;
+                            break;
+                        } else if (!player1.answer.equalsIgnoreCase(movie.name)) {
+                            System.out.println("your answer in not correct" + "\n" + player2.name + " " + "It is your chance now enter you answer:");
+                            player2.answer = input.nextLine();
+                            player1.Points -= 10;
+                            if (player2.answer.equalsIgnoreCase(movie.name)) {
+                                System.out.println("Good guess!!");
+                                gameround++;
+                                break;
+                            } else if (!player2.answer.equalsIgnoreCase(movie.name)) {
+                                System.out.println("Both answers are wrong");
+                                player2.Points -= 10;
+                            }
+                        }
+
+                    }
+
 
                 }
 
-
-            }
+            System.out.println(player1.name + " " + "have" + player1.Points + " " + player2.name + " " + "have" + player2.Points + " ");
+        } else {
+            System.out.println(player1.name + " " + "have" + player1.Points + " " + player2.name + " " + "have" + player2.Points + " ");
         }
-        System.out.println(player1.name+" "+"have"+player1.Points+" "+player2.name+" "+"have" + player2.Points+" ");
+
     }
 }
+
 
 
 
